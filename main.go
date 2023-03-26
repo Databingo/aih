4809b7e858f49cbaa978907edef4358e8070d8df
today072 <databingo@foxmail.com>
2023-03-26 11:07:43 +0800

    update

--- main.go
+++ main.go
@@ -20,6 +20,9 @@ import (
 	"github.com/tidwall/sjson"
 )
 
+
+
+
 func main() {
 
 	// Read json configure
@@ -131,6 +134,7 @@ func main() {
 				//fmt.Println(".key         Set key")
 				fmt.Println(".proxy       Set proxy")
 				fmt.Println(".new         New conversation")
+				fmt.Println(".prompt      Role of Assistant for create precise prompt")
 				fmt.Println(".speak       Voice speak context")
 				fmt.Println(".quiet       Quiet not speak")
 				fmt.Println(".clear       Clear screen")
@@ -146,6 +150,12 @@ func main() {
 				used_tokens = 0
 				left_tokens = max_tokens - used_tokens
 				continue
+			case ".prompt":
+				messages = make([]openai.ChatCompletionMessage, 0)
+				max_tokens = 4097
+				used_tokens = 0
+				left_tokens = max_tokens - used_tokens
+				userInput = prompt_prove 
 			case ".quiet":
 				speak = 0
 				continue
@@ -275,3 +285,7 @@ func main() {
 
 		}
 	}
+
+
+
+var prompt_prove = `I want you to become my Prompt Creator. Your goal is to help me craft the best possible prompt for my needs. The prompt will be used by you, ChatGPT. You will follow the following process: 1. Your first response will be to ask me what the prompt should be about. I will provide my answer, but we will need to improve it through continual iterations by going through the next steps. 2. Based on my input, you will generate 3 sections. a) Revised prompt (provide your rewritten prompt. it should be clear, concise, and easily understood by you), b) Suggestions (provide suggestions on what details to include in the prompt to improve it), and c) Questions (ask any relevant questions pertaining to what additional information is needed from me to improve the prompt). 3. We will continue this iterative process with me providing additional information to you and you updating the prompt in the Revised prompt section until it's complete.`
