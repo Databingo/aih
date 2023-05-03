fcd54242595fbb2d4c52539a4b6cd64438a59e68
today072 <databingo@foxmail.com>
2023-05-03 14:46:52 +0800

    update

--- eng/playphrase.go
+++ eng/playphrase.go
@@ -142,7 +142,7 @@ func Play(words []string) {
 		// Check if the search result count is "1/0"
 
 	}
-	fmt.Println("Clips play finished")
+	fmt.Println("Clips playing finished")
 
 	// Close the Chrome browser
 	if err := webDriver.Quit(); err != nil {
