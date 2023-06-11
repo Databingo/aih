758d919dbb11df55348d245f9d14d7a0525e8105
today072 <databingo@foxmail.com>
2023-06-11 10:22:28 +0800

    update

--- uc.py
+++ uc.py
@@ -129,15 +129,15 @@ while 1:
         if ori:
             try:
                 img = wait.until(EC.presence_of_element_located((By.XPATH,  "//img[contains(@src, 'https://www.gstatic.com/lamda/images/sparkle_resting_v2_1ff6f6a71f2d298b1a31.gif')]")))
-                time.sleep(0.5)
-                print("get img")
+               #time.sleep(0.5)
+               #print("get img")
                #response = img.find_element(By.XPATH,  "./ancestor::model-response")
                 response = img.find_element(By.XPATH,  "ancestor::model-response")
                #response  = driver.find_element(By.XPATH,  "//model-response[last()]")
                #content = response.find_element(By.XPATH, ".//message-content")
                 contents = response.find_elements(By.XPATH, ".//message-content")
                 texts= "\n".join(content.text for content in contents)
-                test = "\n".join(line for line in texts.splitlines() if line)
+                text = "\n".join(line for line in texts.splitlines() if line)
                #text = response.text
                 print(text)
 
