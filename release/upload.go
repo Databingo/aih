f0bac7ebcd432667bd4281846b9542785d8906f0
today072 <databingo@foxmail.com>
2023-08-10 19:20:02 +0800

    update

--- release/upload.go
+++ release/upload.go
@@ -31,10 +31,10 @@ func main() {
 
 	// Create a new release
 	release, _, err := client.Repositories.CreateRelease(ctx, owner, repo, &github.RepositoryRelease{
-		TagName:         github.String("v0.1.3.1"),
+		TagName:         github.String("v0.1.3.2"),
 		TargetCommitish: github.String("master"),
-		Name:            github.String("Release v0.1.3.1"),
-		Body:            github.String("Welcome to Aih! Change ChatGPT web to undetected_chrome mode."),
+		Name:            github.String("Release v0.1.3.2"),
+		Body:            github.String("Welcome to Aih! Integrate HuggingChat in undetected_chromedriver mode; Remove Bing due to the unstable and weak functionality."),
 		Draft:           github.Bool(false),
 		Prerelease:      github.Bool(false),
 	})
