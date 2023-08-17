793acad580657ea0370424a540988b4aee94b556
today072 <databingo@foxmail.com>
2023-08-17 22:45:08 +0800

    update

--- release/upload.go
+++ release/upload.go
@@ -31,10 +31,10 @@ func main() {
 
 	// Create a new release
 	release, _, err := client.Repositories.CreateRelease(ctx, owner, repo, &github.RepositoryRelease{
-		TagName:         github.String("v0.1.4.8"),
+		TagName:         github.String("v0.1.4.9"),
 		TargetCommitish: github.String("master"),
-		Name:            github.String("Release v0.1.4.8"),
-		Body:            github.String("Welcome to Aih! Add 'Unable to load page' detecting in xpath of ChatGPT."),
+		Name:            github.String("Release v0.1.4.9"),
+		Body:            github.String("Welcome to Aih! Fix waiting response xpath of Claude."),
 		Draft:           github.Bool(false),
 		Prerelease:      github.Bool(false),
 	})
