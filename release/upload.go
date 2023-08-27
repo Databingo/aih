4ba80934de611c4dc85fb6e544316eec40d9cf65
today072 <databingo@foxmail.com>
2023-08-27 15:22:15 +0800

    update

--- release/upload.go
+++ release/upload.go
@@ -31,10 +31,10 @@ func main() {
 
 	// Create a new release
 	release, _, err := client.Repositories.CreateRelease(ctx, owner, repo, &github.RepositoryRelease{
-		TagName:         github.String("v0.1.62"),
+		TagName:         github.String("v0.1.6.3"),
 		TargetCommitish: github.String("master"),
-		Name:            github.String("Release v0.1.6.2"),
-		Body:            github.String("Welcome to Aih! Escape from \" in question when sending json to Claude2."),
+		Name:            github.String("Release v0.1.6.3"),
+		Body:            github.String("Welcome to Aih! We have Changed bard's waiting response logic to for-loop for 60 seconds; added wait message such like 'Bard generating...' to every model's interactive process."),
 		Draft:           github.Bool(false),
 		Prerelease:      github.Bool(false),
 	})
