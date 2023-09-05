580ee4f7a5a1b3ed74b3a6dece77ce3ed00ddbb6
today072 <databingo@foxmail.com>
2023-09-05 12:32:02 +0800

    update

--- release/upload.go
+++ release/upload.go
@@ -31,10 +31,10 @@ func main() {
 
 	// Create a new release
 	release, _, err := client.Repositories.CreateRelease(ctx, owner, repo, &github.RepositoryRelease{
-		TagName:         github.String("v0.2.2"),
+		TagName:         github.String("v0.2.3"),
 		TargetCommitish: github.String("master"),
-		Name:            github.String("Release v0.2.2"),
-		Body:            github.String("Welcome to Aih! Fixed Claude json encoding with newline problem."),
+		Name:            github.String("Release v0.2.3"),
+		Body:            github.String("Welcome to Aih! Fixed Claude json encoding with newline problem in multiple input mode."),
 		Draft:           github.Bool(false),
 		Prerelease:      github.Bool(false),
 	})
