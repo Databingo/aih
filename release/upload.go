6fb545cd3d3844654d4b2dafd6b6a40021ea4dd9
today072 <databingo@foxmail.com>
2023-09-08 09:28:39 +0800

    update

--- release/upload.go
+++ release/upload.go
@@ -31,10 +31,10 @@ func main() {
 
 	// Create a new release
 	release, _, err := client.Repositories.CreateRelease(ctx, owner, repo, &github.RepositoryRelease{
-		TagName:         github.String("v0.2.5"),
+		TagName:         github.String("v0.2.5.1"),
 		TargetCommitish: github.String("master"),
-		Name:            github.String("Release v0.2.5"),
-		Body:            github.String("Welcome to Aih! Added Falcon180 model from huggingchat."),
+		Name:            github.String("Release v0.2.5.1"),
+		Body:            github.String("Welcome to Aih! Optimized parsing result logic of llama2 and falcon180."),
 		Draft:           github.Bool(false),
 		Prerelease:      github.Bool(false),
 	})
